16ec2e80d3c2481f9d23889941d3449ee3c484aa
youssefzaguia <youssefultras02tn@gmail.com>
2024-05-20 15:44:21 +0100

    More returns!

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
+    lenth = len(sentence)
     if not sentence:
-        first_char = None
-    else:
-        first_char = sentence[0]
-    sentence_length = len(sentence)
-    return (first_char, sentence_length)
+        return (lenth, None)
+    return (lenth, sentence[0])
