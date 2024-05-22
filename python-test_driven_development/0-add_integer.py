0766aebbb8b8482d9c8e6ad09a110fc14ea9f3a5
youssefzaguia <youssefultras02tn@gmail.com>
2024-05-22 16:20:29 +0100

    Write a function that adds 2 integers.

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def add_integer(a, b=98):
+    """
+    function that adds 2 integers
+    a and b must be integers or floats if not
+    aise a TypeError exception with the message
+    a must be an integer or b must be an integer
+    """
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
